61a5c1a90c44cc44576f342b406ccccf7915c22d
parkhyunwoo <psw1942@chemtopia.net>
2023-11-08 17:31:25 +0900

    시큐리티 적용 및 로그인 구현 + JWT #2

--- hw_back/src/main/java/com/hw/security/SpringSecurityConfig.java
+++ hw_back/src/main/java/com/hw/security/SpringSecurityConfig.java
@@ -5,39 +5,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
-import com.hw.common.JwtAccessDeniedHandler;
-import com.hw.common.JwtAuthenticationEntryPoint;
 import com.hw.common.JwtSecurityConfig;
-import com.hw.common.TokenProvide;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
 	
-	private final TokenProvide tokenProvide;
-    private final CorsFilter corsFilter;
-    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+	private final JwtTokenProvider jwtTokenProvider;
+	 
+    public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
     
-    public SpringSecurityConfig(
-		TokenProvide tokenProvide,
-		CorsFilter corsFilter,
-		JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
-		JwtAccessDeniedHandler jwtAccessDeniedHandler
-	
-	){
-        this.tokenProvide = tokenProvide;
-        this.corsFilter = corsFilter;
-        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
-        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
-	}
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
 	
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -45,8 +32,6 @@ public class SpringSecurityConfig {
         http
         	.csrf((csrf) ->
         		csrf.disable())
-        	.cors((cors) ->
-        		cors.configurationSource(corsConfigurationSource()))
         	.authorizeHttpRequests((authorizeRequests) ->
         		authorizeRequests
         			.requestMatchers("/user/*").permitAll()
@@ -64,23 +49,5 @@ public class SpringSecurityConfig {
         	
 		return http.build();
 	}
-
-	@Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("OPTIONS");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("DELETE");
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        return source;
-    }
     
 }
